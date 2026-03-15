b935b2d0e20fe581f2e5d9dc796bad69c22256ad
Rowan Manning <138944+rowanmanning@users.noreply.github.com>
2026-03-15 21:49:42 +0000

    refactor: reorganise module to make testing easier

---
+++ packages/node-versions/lib/get-package-node-versions.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const { getEnginesNodeVersions } = require('./get-engines-node-versions.js');
+
+/**
+ * @import nodeVersions from '@rowanmanning/node-versions'
+ */
+
+/** @type {nodeVersions['getPackageNodeVersions']} */
+exports.getPackageNodeVersions = function getPackageNodeVersions(pkg, options) {
+	/** @type {any} */
+	let engines;
+
+	// We can only extract engines data from a v2+ lockfile
+	if (typeof pkg?.lockfileVersion === 'number' && pkg.lockfileVersion > 1) {
+		engines = pkg?.packages?.['']?.engines?.node;
+	} else {
+		engines = pkg?.engines?.node;
+	}
+
+	if (typeof engines !== 'string') {
+		return [];
+	}
+	return getEnginesNodeVersions(engines, options);
+};
